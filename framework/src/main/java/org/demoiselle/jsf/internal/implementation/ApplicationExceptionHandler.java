8a663f00c9dce14c4be209561a00330950326721
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2016-05-19 17:06:33 -0300

    Adição de alguns mecanismos de segurança

---
+++ framework/src/main/java/org/demoiselle/jsf/internal/implementation/ApplicationExceptionHandler.java
@@ -0,0 +1,122 @@
+/*
+ * Demoiselle Framework
+ * Copyright (C) 2010 SERPRO
+ * ----------------------------------------------------------------------------
+ * This file is part of Demoiselle Framework.
+ *
+ * Demoiselle Framework is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License version 3
+ * along with this program; if not,  see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ * ----------------------------------------------------------------------------
+ * Este arquivo é parte do Framework Demoiselle.
+ *
+ * O Framework Demoiselle é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
+ * do Software Livre (FSF).
+ *
+ * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
+ * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
+ * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
+ * para maiores detalhes.
+ *
+ * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
+ * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
+ * ou escreva para a Fundação do Software Livre (FSF) Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
+ */
+package org.demoiselle.jsf.internal.implementation;
+
+import org.demoiselle.exception.ApplicationException;
+import org.demoiselle.exception.DemoiselleException;
+import org.demoiselle.jsf.internal.configuration.ExceptionHandlerConfig;
+import org.demoiselle.jsf.util.Faces;
+import org.demoiselle.jsf.util.PageNotFoundException;
+import org.demoiselle.jsf.util.Redirector;
+
+import javax.enterprise.inject.spi.CDI;
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author SERPRO
+ */
+public class ApplicationExceptionHandler extends AbstractExceptionHandler {
+
+    public ApplicationExceptionHandler(final ExceptionHandler wrapped) {
+        super(wrapped);
+    }
+
+    protected boolean handleException(final Throwable cause, FacesContext facesContext) {
+        boolean handled = false;
+        ExceptionHandlerConfig config = CDI.current().select(ExceptionHandlerConfig.class).get();// Beans.getReference(ExceptionHandlerConfig.class);
+
+        if (config.isApplicationExceptionHandle() && isApplicationException(cause)) {
+
+            if (isRendering(facesContext)) {
+                handled = handlingDuringRenderResponse(cause, config);
+            } else {
+                Faces.addMessage(cause);
+                handled = true;
+            }
+        }
+
+        return handled;
+    }
+
+    private boolean isRendering(FacesContext context) {
+        return PhaseId.RENDER_RESPONSE.equals(context.getCurrentPhaseId());
+    }
+
+    /**
+     * In render response phase an exception interrupt the renderization. So this method will redirect the rendering to
+     * an page configured in demoiselle.properties
+     *
+     * @param cause
+     * @param config
+     * @return
+     */
+    private boolean handlingDuringRenderResponse(final Throwable cause, final ExceptionHandlerConfig config) {
+        boolean handled = false;
+        try {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("exception", cause.getMessage());
+            Redirector.redirect(config.getDefaultRedirectExceptionPage(), map);
+            handled = true;
+        } catch (PageNotFoundException ex) {
+            // TODO Colocar a mensagem no bundle
+            throw new DemoiselleException(
+                    "A tela de exibição de erros: \""
+                            + ex.getViewId()
+                            + "\" não foi encontrada. Caso o seu projeto possua outra, defina no arquivo de configuração a chave \""
+                            + "frameworkdemoiselle.exception.default.redirect.page" + "\"", ex);
+        }
+        return handled;
+    }
+
+    protected Throwable getRoot(final Throwable throwable) {
+        Throwable root = throwable;
+
+        while (root != null && !isApplicationException(root)) {
+            root = root.getCause();
+        }
+
+        return root;
+    }
+
+    private static boolean isApplicationException(final Throwable throwable) {
+        return throwable != null && throwable.getClass().isAnnotationPresent( ApplicationException.class);
+    }
+}
---
+++ framework/src/main/java/org/demoiselle/jsf/internal/implementation/SecurityObserver.java
@@ -0,0 +1,173 @@
+/*
+ * Demoiselle Framework
+ * Copyright (C) 2010 SERPRO
+ * ----------------------------------------------------------------------------
+ * This file is part of Demoiselle Framework.
+ *
+ * Demoiselle Framework is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License version 3
+ * along with this program; if not,  see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ * ----------------------------------------------------------------------------
+ * Este arquivo é parte do Framework Demoiselle.
+ *
+ * O Framework Demoiselle é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
+ * do Software Livre (FSF).
+ *
+ * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
+ * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
+ * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
+ * para maiores detalhes.
+ *
+ * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
+ * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
+ * ou escreva para a Fundação do Software Livre (FSF) Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
+ */
+package org.demoiselle.jsf.internal.implementation;
+
+import org.demoiselle.annotation.Name;
+import org.demoiselle.configuration.ConfigurationException;
+import org.demoiselle.jsf.internal.configuration.JsfSecurityConfig;
+import org.demoiselle.jsf.util.PageNotFoundException;
+import org.demoiselle.jsf.util.Redirector;
+import org.demoiselle.security.AfterLoginSuccessful;
+import org.demoiselle.security.AfterLogoutSuccessful;
+import org.demoiselle.util.ResourceBundle;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.CDI;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author SERPRO
+ */
+@SessionScoped
+public class SecurityObserver implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private transient JsfSecurityConfig config;
+
+    private transient Map<String, Object> savedParams;
+
+    private String savedViewId;
+
+//    @Inject
+//    private Logger logger;
+
+    @Inject
+    @Name("demoiselle-jsf-bundle")
+    private ResourceBundle bundle;
+
+    public SecurityObserver() {
+        clear();
+    }
+
+    private Map<String, Object> getSavedParams() {
+        if (this.savedParams == null) {
+            this.savedParams = new HashMap<String, Object>();
+        }
+
+        return this.savedParams;
+    }
+
+    public JsfSecurityConfig getConfig() {
+        if (this.config == null) {
+            this.config = CDI.current().select(JsfSecurityConfig.class).get();// Beans.getReference(JsfSecurityConfig.class);
+        }
+
+        return this.config;
+    }
+
+    private void saveCurrentState() {
+        clear();
+        FacesContext facesContext = FacesContext.getCurrentInstance();//CDI.current().select(FacesContext.class).get();// Beans.getReference(FacesContext.class);
+
+        if (!getConfig().getLoginPage().equals(facesContext.getViewRoot().getViewId())) {
+            getSavedParams().putAll(facesContext.getExternalContext().getRequestParameterMap());
+            savedViewId = facesContext.getViewRoot().getViewId();
+        }
+    }
+
+    public void redirectToLoginPage() {
+        saveCurrentState();
+
+        try {
+            Redirector.redirect(getConfig().getLoginPage());
+
+        } catch (PageNotFoundException cause) {
+            throw new ConfigurationException(bundle.getString("login-page-not-found", cause.getViewId()), cause);
+        }
+    }
+
+    public void onLoginSuccessful(@Observes final AfterLoginSuccessful event) {
+        boolean redirectedFromConfig = false;
+
+        try {
+            if (savedViewId != null) {
+                Redirector.redirect(savedViewId, getSavedParams());
+
+            } else if (getConfig().isRedirectEnabled()) {
+                redirectedFromConfig = true;
+                Redirector.redirect(getConfig().getRedirectAfterLogin(), getSavedParams());
+            }
+
+        } catch (PageNotFoundException cause) {
+            if (redirectedFromConfig) {
+                throw new ConfigurationException(bundle.getString("after-login-page-not-found", cause.getViewId()),
+                        cause);
+            } else {
+                throw cause;
+            }
+
+        } finally {
+            clear();
+        }
+    }
+
+    public void onLogoutSuccessful(@Observes final AfterLogoutSuccessful event) {
+        try {
+            if (getConfig().isRedirectEnabled()) {
+                Redirector.redirect(getConfig().getRedirectAfterLogout());
+            }
+
+        } catch (PageNotFoundException cause) {
+            throw new ConfigurationException(bundle.getString("after-logout-page-not-found", cause.getViewId()), cause);
+
+        } finally {
+            try {
+                HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext()
+                        .getSession(false);
+                session.invalidate();
+            } catch (IllegalStateException e) {
+//                logger.fine("Esta sessão já foi invalidada.");
+            }
+        }
+    }
+
+    private void clear() {
+        savedViewId = null;
+        getSavedParams().clear();
+    }
+
+}
+
